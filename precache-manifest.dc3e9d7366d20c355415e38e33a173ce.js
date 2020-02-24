47b939cdec3d4f910d89c1d4d1e5f25afcb61e82
Adams Adeyiga <jadams@calminfotech.com>
2020-02-24 13:13:34 +0100

    Updates

---
+++ precache-manifest.dc3e9d7366d20c355415e38e33a173ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2a6284e09cb8d7a6b46ef81822ce2d",
+    "url": "/calm-global/index.html"
+  },
+  {
+    "revision": "1a667e488bd903022012",
+    "url": "/calm-global/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e2c0863b18f4056df8b8",
+    "url": "/calm-global/static/js/2.65fb34a8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calm-global/static/js/2.65fb34a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a667e488bd903022012",
+    "url": "/calm-global/static/js/main.6c416860.chunk.js"
+  },
+  {
+    "revision": "87a845ebfc7dc0a36f16",
+    "url": "/calm-global/static/js/runtime-main.1b102bee.js"
+  }
+]);
